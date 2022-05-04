59fa374e170ff45f8a90531d8b2f541794a3beda
TathoPlox <tathoplox@gmail.com>
2022-05-04 15:55:26 -0500

    4 may

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 function numeroDado(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -13,3 +14,42 @@ for (let i = 1; i <= dados; i++) {
     break;
   }
 }
+*/
+
+class Oficio {
+  constructor(off, dmg, def, agi) {
+    this.off = off.toUpperCase();
+    this.dmg = parseFloat(dmg);
+    this.def = parseFloat(def);
+    this.agi = parseFloat(agi);
+  }
+}
+
+const oficio = [];
+
+oficio.push(new Oficio("guerrero", 3, 2, 2));
+oficio.push(new Oficio("paladin", 2, 4, 1));
+oficio.push(new Oficio("peleador", 3, 1, 3));
+
+let busca = prompt("¿Cuál es tu oficio? guerrero, paladin o peleador");
+
+let character = oficio.find((item) => item.off == busca.toUpperCase());
+
+/*if (busca == oficio.off) {
+  alert("verdadero");
+} else {
+  alert("falso");
+}
+*/
+document.write(
+  "<h1>Tu oficio es: " +
+    character.off +
+    "</h1>" +
+    "<p> Daño: " +
+    character.dmg +
+    "<br> Defensa: " +
+    character.def +
+    "<br> Agilidad: " +
+    character.agi +
+    "</p>"
+);
